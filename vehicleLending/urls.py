c07772d61d03d0e55842c3615b6c647e06ab3211
colbycheese55 <112818133+colbycheese55@users.noreply.github.com>
2025-03-14 21:40:09 -0400

    working signout button

--- vehicleLending/urls.py
+++ vehicleLending/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.login_view, name='login'),  # the index page
     path('dashboard/patron', views.patron_dashboard, name='patron_dashboard'),
     path('dashboard/librarian', views.librarian_dashboard, name='librarian_dashboard'),
-    path('sign-out', views.sign_out, name='sign_out'),
+    path('sign_out', views.sign_out, name='sign_out'),
     path('auth-receiver', views.auth_receiver, name='auth_receiver'),
     path('home', views.select_collection, name='select_collection'),
 ]
