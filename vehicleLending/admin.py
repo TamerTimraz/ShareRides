1ed4d351a271d7c2f080ee7c17fb05f7f0aaa5fe
TamerTimraz <tamertimraz04@gmail.com>
2025-03-14 20:53:06 -0400

    registered Vehicle to admin site

--- vehicleLending/admin.py
+++ vehicleLending/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from vehicleLending.models import User
+from vehicleLending.models import User, Vehicle
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Vehicle)
