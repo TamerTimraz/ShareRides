7a3418c3c3da97737b25b4c406884e6b907badba
shawnt1790 <shawnreby04@gmail.com>
2025-04-29 23:07:40 -0400

    Fixed Notifications popping up perpetually

--- vehicleLending/context_processors.py
+++ vehicleLending/context_processors.py
@@ -1,15 +1,21 @@
-# vehicleLending/context_processors.py
-
 from .models import BorrowRequest
 
 def borrow_request_notifications(request):
-    # only care about logged-in patrons
     if not request.user.is_authenticated:
         return {}
 
-    count = BorrowRequest.objects.filter(
+    # Pull any newly accepted OR denied requests that we haven’t yet notified the user about
+    # (we’ll keep a list of their IDs in the session)
+    seen = request.session.get('borrow_notified_ids', [])
+    qs = BorrowRequest.objects.filter(
         requester=request.user,
-        status__in=['accepted','denied']
-    ).count()
+        status__in=['accepted', 'denied']
+    ).exclude(id__in=seen)
 
-    return {'borrow_notification_count': count}
+    count = qs.count()
+    if count:
+        # record these so we don’t notify again
+        request.session['borrow_notified_ids'] = seen + list(qs.values_list('id', flat=True))
+        # pass count into the template
+        return {'borrow_notification_count': count}
+    return {}
